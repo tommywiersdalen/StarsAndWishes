2e93fde7df2565b19990df81beeae4c429ac8300
tjommyboy Wiersdalen <tommywiersdalen@gmail.com>
2023-03-20 13:04:00 +0100

    added index page and basic styles

--- app/routes/__app/index.tsx
+++ app/routes/__app/index.tsx
@@ -1,3 +1,36 @@
+import { Link } from "@remix-run/react";
+import { GiMagicLamp, GiCursedStar } from "react-icons/gi";
+import dragon from "../../../public/images/pngegg.png";
+
 export default function Index() {
-	return <p>Home</p>;
+	return (
+		<section>
+			<div className="flex flex-col items-center justify-center mx-auto mt-12 max-w-xl">
+				<h1 className="text-4xl text-white font-bold tracking-wider mb-6">
+					Welcome Wildlings!
+				</h1>
+				<p
+					className="text-white text-xl px-4
+                ">
+					This is the stars and wishes page. Here you can submit your stars and
+					whishes to post session to your dungeon master. Just click the button
+					and send away!
+				</p>
+				<Link
+					to="/"
+					className="flex items-center mt-10 p-2 px-8 text-white bg-cyan-800 rounded-lg hover:bg-cyan-500 duration-200 focus:outline-none">
+					<GiMagicLamp size={40} />
+					<span className="mx-2">Submit answers!</span>
+					<GiCursedStar size={40} />
+				</Link>
+			</div>
+			<div className="flex flex-col items-center mx-md mx-auto justify-center mt-3">
+				<img
+					src={`${dragon}`}
+					alt=""
+					className="opacity-50 animate-spin-slow max-w-md"
+				/>
+			</div>
+		</section>
+	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./app/**/*.{ts,tsx,jsx,js}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'spin-slow': 'spin 60s linear infinite',
+      }
+    },
   },
   plugins: [],
 }
