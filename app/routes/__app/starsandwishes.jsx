de0e2537a1cbf40893a363d4d87cd4048b87d91b
tjommyboy Wiersdalen <tommywiersdalen@gmail.com>
2023-03-22 12:00:09 +0100

    add +

--- app/routes/__app/starsandwishes.jsx
+++ app/routes/__app/starsandwishes.jsx
@@ -9,7 +9,7 @@ export default function StarsAndWishesLayout() {
 					<Link
 						to="add"
 						className="flex items-center mt-10 p-2 px-8 text-white bg-cyan-800 rounded-lg hover:bg-cyan-500 duration-200 focus:outline-none">
-						<span className="mx-2">Add</span>
+						<span className="mx-2">Add +</span>
 					</Link>
 				</div>
 			</section>
